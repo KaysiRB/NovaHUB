9ddfdc546951cef9de5a8612e033450ffec57a1a
Nova / Kays <165211324+KaysiRB@users.noreply.github.com>
2024-11-17 23:26:54 +0100

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -1,27 +1,38 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Auto Play Piano V1 - Made By NovaOT (Beta Version)", "BloodTheme")
 
--- Tab Auto Piano
 local Tab = Window:NewTab("Auto Piano")
 local Section = Tab:NewSection("Auto Piano")
-
--- Bouton pour jouer "AOT"
 Section:NewButton("AOT", "Autoplayf", function()
-    -- Démarre l'autoplay du piano avec la partition définie dans Player.lua
-    shared.stop = true  -- Permet d'arrêter à tout moment si true
-    -- CONFIG
-    shared.ftime = 2*60 + 00 -- temps en secondes pour finir la chanson
-    shared.delay = nil -- délai surclasse le ftime
-    shared.tempo = nil -- tempo surclasse le délai
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 2*60 + 00 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- tempo overides the delay
 
-    -- Définir la partition
-    shared.scr = [[ 
-        6 | p s [quf] | [wy] - [ak] [sl] [eud] | [0oh] a [qtp] | ...
-        [9rp] a [0ps] [dz] f - j - [wpd] - [Qaf] k l z -
-        ]] -- Remplacer par la partition réelle
+shared.scr = [[ [6s] 0 [ts] e [us] t [of] u [4j] 8 [ej] q [tj] e [yh] u
+[8f] w [uf] t [of] u [tf] w [5a] 9 [ra] w [ya] r [wp] 0
 
-    -- Charger Player.lua pour jouer la partition
-    loadstring(game:HttpGet("https://github.com/KaysiRB/SMTHNG/raw/refs/heads/main/Player.lua"))()
+[6e] s [es] p [us] p [es] d [4f] 8 [qs] s [qo] f q
+[8t] w t w t [ws] [ts] [ws] [5a] 9 w [ra] [wa] [rp] [ws] u
+[6t] s [es] p [us] p [es] d [4f] u [qs] s [qoh] f q
+[8t] t t s [tus] d [5w] [wy] [wy] f f f
+[6us] 0 [ts] e [us] t [of] u [4sj] 8 [ej] q [tj] [ef] [fh] f
+[8of] w [uf] t [of] u [tf] w [5oa] 9 [ra] w [ya] [rf] [wf] [rf]
+[6us] 0 t e [ud] [td] [od] f [4d] [8s] [es] q t [ef] [yf] f
+[8of] w u t o [us] s [ts] [5oa] [9a] [ra] [ws] u t e w
+f f f [6f] u [ps] e 6 p e s [4qp] o q q
+s [ish] [osh] [f8] u o u 8 [us] [8oh] [uh] [of] 5 d 5
+5 [of] [of] [of] [6f] u [ps] e 6 [es] [us] [ep] [4g] q u q 4 q u
+h [8sf] h [osf] [8h] [osf] h [8of] d g 5 9 w p g w [of] w
+
+[6us] 0 [ts] e [us] [tf] [sfl] [adk] [4sj] 8 [ej] [qp]
+[tj] f [th] d [8of] w [uf] t [oh] [ug] [tf] d [5oa] 9 r [wo]
+[oag] r [wof] d [6us] 0 [ts] e [us] [tf] [sfl] [adk] [4sj] 8 [ej] [qp]
+[tj] [ef] [yh] d [8of] w [uf] t [oh] [ug] [tf] d [5od] 9 r [wo]
+[oag] r [wof] d 6 0 e t o ]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JxcExploit/AutoPlayPianoScript/main/NewAutoPlayPiano"))()
 end)
 
 local Tab = Window:NewTab("Stop Piano")
