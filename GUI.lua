189f9ff4facd35b2a5f7cb94643c26572dd41d64
Nova / Kays <165211324+KaysiRB@users.noreply.github.com>
2024-11-17 23:38:59 +0100

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -12,32 +12,29 @@ Section:NewButton("AOT", "Autoplayf", function()
     shared.delay = nil -- delay overides the ftime
     shared.tempo = nil -- tempo overides the delay
     
-    shared.scr = 
-    [[ 
-        
-    [6s] 0 [ts] e [us] t [of] u [4j] 8 [ej] q [tj] e [yh] u
-    [8f] w [uf] t [of] u [tf] w [5a] 9 [ra] w [ya] r [wp] 0
-    
-    [6e] s [es] p [us] p [es] d [4f] 8 [qs] s [qo] f q
-    [8t] w t w t [ws] [ts] [ws] [5a] 9 w [ra] [wa] [rp] [ws] u
-    [6t] s [es] p [us] p [es] d [4f] u [qs] s [qoh] f q
-    [8t] t t s [tus] d [5w] [wy] [wy] f f f
-    [6us] 0 [ts] e [us] t [of] u [4sj] 8 [ej] q [tj] [ef] [fh] f
-    [8of] w [uf] t [of] u [tf] w [5oa] 9 [ra] w [ya] [rf] [wf] [rf]
-    [6us] 0 t e [ud] [td] [od] f [4d] [8s] [es] q t [ef] [yf] f
-    [8of] w u t o [us] s [ts] [5oa] [9a] [ra] [ws] u t e w
-    f f f [6f] u [ps] e 6 p e s [4qp] o q q
-    s [ish] [osh] [f8] u o u 8 [us] [8oh] [uh] [of] 5 d 5
-    5 [of] [of] [of] [6f] u [ps] e 6 [es] [us] [ep] [4g] q u q 4 q u
-    h [8sf] h [osf] [8h] [osf] h [8of] d g 5 9 w p g w [of] w
-    
-    [6us] 0 [ts] e [us] [tf] [sfl] [adk] [4sj] 8 [ej] [qp]
-    [tj] f [th] d [8of] w [uf] t [oh] [ug] [tf] d [5oa] 9 r [wo]
-    [oag] r [wof] d [6us] 0 [ts] e [us] [tf] [sfl] [adk] [4sj] 8 [ej] [qp]
-    [tj] [ef] [yh] d [8of] w [uf] t [oh] [ug] [tf] d [5od] 9 r [wo]
-    [oag] r [wof] d 6 0 e t o 
-        
-    ]]
+    shared.scr = [[ 6 | p s [quf] | [wy] - [ak] [sl]
+    [eud] | [0oh] a [qtp] | [wy] - [dz] -
+    [euoh] | [0tp] a [qof] | [wy] - [ak] [sl]
+    [eod] - s | [0oah] d [qt] s | [0r] - [pj] [dz]
+    [eu] [sl] | 6 - 6-e 0 6 - 6-[epj] f
+    [qtyf] | f-k-[wry] | j-l-[eu] | j f
+    [qwtf] | f-k-[wty] | l-f-[eu] | i - [uj] f
+    [qtif] | f-[ak]-[wyo] | [sl]-[oh]-[eu] | y - [uoh] f-[oh]-
+    [qe] | [wt] - [dz] l-[dz]-[eu] | [fl]-j |
+    [eua] - [quip] | [9sh] - [8af] | {ghjhjf} -
+    [Epd] | {flkjflkjflkjflkj} [eos] - {zhzlkl}-h-z-j-l-h-k-f-
+    [9rp] a [0ps] [dz] f - j - [wpd] - [Qaf] k l z -
+    [0p]-[ed]-[ys]-a-[0p]-[ed]-[rs]-a-[0p]-[ed]-[ts]-a-[0p]-[ed]-[ts]-a-
+    [0p]-[ed]-[ts]-a-[ep]-[rd]-[0s]-[ta]-p-[yd]-[ts]-a-p-[ed]-[rs]-a-[0p]-[ed]-[ts] |
+    f [dz] [4djz] [8sl] [qsl] [8h] [4sfl] 8 [wak] [8sl]
+    6 0 e 0 e 0 [6f] [0dz] [4dfz] [8sl] [qoh] [8f] [4dhz] 8 [wf] [8sl]
+    6 0 e 0 e 0 [6f] [0dz] [4dz] [8sl] [qsl] [8h] [4h] 8 [wdz] [8sl]
+    6 0 e 0 r 0 [tpj] [0sl] [4dz] 8 [qdz] [8sl] [4dz] [8f] [wdz] [8dz]
+    6 [0sl] e 0 6 l k f-j-4 8 q 8 5 [9l] [wk] [9f]-[sl]-
+    6 0 e 0 e [0l] [6k] [0f]-j-4 8 q [8j]-k-5 9 w [9z]-l-
+    6 0 e 0 e [0l] [6k] [0f]-j-4 8 q 8 5 [9l] [wk] [9f]-[sl]-
+    6 0 e 0 r [0l] [tk] [0l]-z-4 8 q [8fx]-[fx]-5 9 w [9l]-k-
+    6-p-[0d]-s-[ea]-p-[0d]-s-[ea]-p-[0d]-s [ep] [0a]-s-6 ]]
     
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KaysiRB/SMTHNG/refs/heads/main/Player.lua"))()
 end)
