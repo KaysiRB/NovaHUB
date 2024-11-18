43b6d29f99dfc0c865a8d7dc101f48ac94164c7e
Nova / Kays <165211324+KaysiRB@users.noreply.github.com>
2024-11-18 20:53:47 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,3 @@
-local NovaGui = loadstring(game:HttpGet("https://raw.githubusercontent.com/KaysiRB/SMTHNG/refs/heads/main/Nova-UI-LIB.lua"))()
+local NovaGui = loadstring(game:HttpGet("https://raw.githubusercontent.com/KaysiRB/SMTHNG/refs/heads/BASE/Module/Nova-UI-LIB.lua"))()
 local gui = NovaGui:CreateGui()
 NovaGui:ToggleGui(gui)
