d63c8ea93f67106760e008f545505b40ec299ffc
Nova / Kays <165211324+KaysiRB@users.noreply.github.com>
2024-11-19 20:12:28 +0100

    Update PianoTest.lua

--- Script/PianoTest.lua
+++ Script/PianoTest.lua
@@ -5,52 +5,44 @@ local Tab = Window:NewTab("Auto Piano")
 local Section = Tab:NewSection("Auto Piano")
 
 local PressureTime = {
-    [""] = 15,   -- 0.15 seconds
-    [' '] = 30,  -- 0.30 seconds
-    ['-'] = 60,  -- 0.60 seconds
-    ['|'] = 240  -- 2.40 seconds
+    [""] = 15,  -- 0.15 seconds
+    [' '] = 30, -- 0.30 seconds
+    ['-'] = 60, -- 0.60 seconds
+    ['|'] = 240 -- 2.40 seconds
 }
 
-Section:NewButton("Start AutoPlay", "Start playing the sheet", function()
-    shared.stop = true
-    shared.ftime = 2 * 60 + 00
+Section:NewButton("Play Piano", "Start autoplay", function()
+    shared.stop = false
+    shared.ftime = 120
     shared.delay = nil
     shared.tempo = nil
-    
     shared.PressureTime = PressureTime
-    
-    shared.scr = [[ 6 | p s [quf] | [wy] - [ak] [sl]
-    [eud] | [0oh] a [qtp] | [wy] - [dz] - ]]
+    shared.scr = [[ q w [er] t y | [ak] [sl] ]]
     
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KaysiRB/SMTHNG/refs/heads/BASE/Module/Player.lua"))()
 end)
 
-local Tab = Window:NewTab("Stop Piano")
-local Section = Tab:NewSection("Stop Piano")
-Section:NewButton("Stop Piano", "Stops current playback", function()
+local StopTab = Window:NewTab("Stop Piano")
+local StopSection = StopTab:NewSection("Stop Piano")
+StopSection:NewButton("Stop", "Stop the autoplay", function()
     shared.stop = true
-    shared.scr = ""
 end)
 
-local Tab = Window:NewTab("Pressure Time Settings")
-local Section = Tab:NewSection("Adjust Pressure Time")
+local AdjustTab = Window:NewTab("Adjust Pressure Time")
+local AdjustSection = AdjustTab:NewSection("Adjust Settings")
 
-Section:NewSlider("Space Delay", "Delay for space", 300, 0, function(value)
-    PressureTime[' '] = value
+AdjustSection:NewSlider("Space Delay", "Adjust delay for space", 300, 0, function(val)
+    PressureTime[" "] = val
 end)
 
-Section:NewSlider("Dash Delay", "Delay for dash (-)", 600, 0, function(value)
-    PressureTime['-'] = value
+AdjustSection:NewSlider("Dash Delay", "Adjust delay for dash", 600, 0, function(val)
+    PressureTime["-"] = val
 end)
 
-Section:NewSlider("Pipe Delay", "Delay for pipe (|)", 2400, 0, function(value)
-    PressureTime['|'] = value
+AdjustSection:NewSlider("Pipe Delay", "Adjust delay for pipe", 2400, 0, function(val)
+    PressureTime["|"] = val
 end)
 
-Section:NewSlider("Empty Delay", "Delay for empty key", 150, 0, function(value)
-    PressureTime[''] = value
+AdjustSection:NewSlider("Empty Delay", "Adjust delay for empty", 150, 0, function(val)
+    PressureTime[""] = val
 end)
-
-local Tab = Window:NewTab("Custom Sheets")
-local Section = Tab:NewSection("Create Your Sheet")
-Section:NewButton("Work In Progress", "Custom sheet functionality is coming", function() end)
