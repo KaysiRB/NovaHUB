c6d5c9afb6ca930448f121c4d10a791908c7d325
Nova / Kays <165211324+KaysiRB@users.noreply.github.com>
2024-11-24 21:48:11 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,6 +5,7 @@ print("N  NN   o o   v v   a a   H  H   U   U   B   B")
 print("N   N   ooo    v    aaa   H  H    UUU    BBBB ")
 
 local Luminosity = loadstring(game:HttpGet("https://raw.githubusercontent.com/KaysiRB/NovaHUB/refs/heads/main/MODULE/Nova-UI-LIB.lua"))()
+Luminosity:LoadingScreen()
 local Window = Luminosity.new("Nova HUB", "v0.5.0", 4370345701)
 
 local Home = Window.Tab("Home", 6026568198)
