5a2fd93bf67fed17153ce0126d4b788df9c6339c
Nova / Kays <165211324+KaysiRB@users.noreply.github.com>
2024-11-24 17:22:39 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,8 @@
-print(" _   _                   _   _ _   _ ____  ")
-print("| \ | | _____   ____ _  | | | | | | | __ ) ")
-print("|  \| |/ _ \ \ / / _` | | |_| | | | |  _ \ ")
-print("| |\  | (_) \ V / (_| | |  _  | |_| | |_) |")
-print("|_| \_|\___/ \_/ \__,_| |_| |_|\___/|____/ ")
+print(" _   _                      _   _  _   _  ____  ")
+print("| \ | |  ___ __   __ __ _  | | | || | | || __ ) ")
+print("|  \| | / _ \\ \ / // _` | | |_| || | | ||  _ \ ")
+print("| |\  || (_) |\ V /| (_| | |  _  || |_| || |_) |")
+print("|_| \_| \___/  \_/  \__,_| |_| |_| \___/ |____/ ")
 
 local Luminosity = loadstring(game:HttpGet("https://raw.githubusercontent.com/KaysiRB/NovaHUB/refs/heads/main/MODULE/Nova-UI-LIB.lua"))()
 local Window = Luminosity.new("Nova HUB", "v0.5.0", 4370345701)
