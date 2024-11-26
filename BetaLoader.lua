5183b7e3982825588f4e2a16358001edebe79606
Nova / Kays <165211324+KaysiRB@users.noreply.github.com>
2024-11-26 19:49:42 +0100

    Update BetaLoader.lua

--- BetaLoader.lua
+++ BetaLoader.lua
@@ -1,1 +1,12 @@
+local MacLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/KaysiRB/NovaHUB/refs/heads/main/MODULE/Nova-UI-LIB-BETA.lua"))()
 
+local Window = MacLib:Window({
+    Title = "Nova HUB",
+    Subtitle = "Beta | V0.50",
+    Size = UDim2.fromOffset(868, 650),
+    DragStyle = 1,
+    DisabledWindowControls = {},
+    ShowUserInfo = true,
+    Keybind = Enum.KeyCode.LeftAlt,
+    AcrylicBlur = true,
+})
