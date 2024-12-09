3ae78181e6e1fb8410b8ead9ea12314ef072e8a0
Nova / Kays <165211324+KaysiRB@users.noreply.github.com>
2024-12-09 23:57:37 +0100

    Update BetaLoader.lua

--- BetaLoader.lua
+++ BetaLoader.lua
@@ -137,7 +137,7 @@ local Tab = TabGroup:Tab({
 
 local Tab = TabGroup:Tab({
     Name = "Visual",
-    Image = "rbxassetid://6026568198" -- Image can be at maximum 16 pixels wide and 16 pixels tall.
+    Image = "rbxassetid://102605267990029" -- Image can be at maximum 16 pixels wide and 16 pixels tall.
 })
 	local Section = Tab:Section({
 		Side = "Left"
