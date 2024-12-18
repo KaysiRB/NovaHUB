b66a225da0faae49ec0b97553f534b4c99138f15
Nova / Kays <165211324+KaysiRB@users.noreply.github.com>
2024-12-18 18:25:41 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -152,12 +152,6 @@ local PlayerTab = TabGroup2:Tab({
 			end,
 		})
 
--- Mettre à jour le vol à chaque frame
-game:GetService("RunService").RenderStepped:Connect(function()
-    FlyModule:updateFlight()  -- Met à jour la position et l'orientation du joueur en vol
-end)
-
-
 local VisualTab = TabGroup2:Tab({
     Name = "Visual",
     Image = "rbxassetid://73762068715433" -- Image can be at maximum 16 pixels wide and 16 pixels tall.
@@ -207,15 +201,74 @@ local PianoTab = TabGroup3:Tab({
 		Section:Header({
 			Text = "Settings"
 		})
+		Section:Header({
+			Text = "Piano"
+		})
+		Section:Button({
+			Name = "Stop Piano",
+            Callback = function()
+                shared.stop = true -- stops the player at any time if true
+                -- CONFIG:
+                shared.delay = nil -- delay overides the ftime
+                shared.tempo = nil -- tempo overides the delay
+
+                shared.scr = [[ e ]]
+
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/KaysiRB/NovaHUB/refs/heads/main/SCRIPT/HomeMadePiano.lua"))()
+            end,
+		})
 	local Section = PianoTab:Section({
 		Side = "Right"
 	})
 		Section:Header({
-			Text = "Music List"
+			Text = "Play Music"
+		})
+		Section:Button({
+			Name = "Faded",
+	        Callback = function()
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 2*60 + 00 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- tempo overides the delay
+Window:Notify({
+    Title = "Nova HUB",
+    Description = "Playing Faded by Alan Walker.",
+    Lifetime = 3
+})
+shared.scr = [[ [6s] 0 [ts] e [us] t [of] u [4j] 8 [ej] q [tj] e [yh] u
+[8f] w [uf] t [of] u [tf] w [5a] 9 [ra] w [ya] r [wp] 0
+
+[6e] s [es] p [us] p [es] d [4f] 8 [qs] s [qo] f q
+[8t] w t w t [ws] [ts] [ws] [5a] 9 w [ra] [wa] [rp] [ws] u
+[6t] s [es] p [us] p [es] d [4f] u [qs] s [qoh] f q
+[8t] t t s [tus] d [5w] [wy] [wy] f f f
+[6us] 0 [ts] e [us] t [of] u [4sj] 8 [ej] q [tj] [ef] [fh] f
+[8of] w [uf] t [of] u [tf] w [5oa] 9 [ra] w [ya] [rf] [wf] [rf]
+[6us] 0 t e [ud] [td] [od] f [4d] [8s] [es] q t [ef] [yf] f
+[8of] w u t o [us] s [ts] [5oa] [9a] [ra] [ws] u t e w
+f f f [6f] u [ps] e 6 p e s [4qp] o q q
+s [ish] [osh] [f8] u o u 8 [us] [8oh] [uh] [of] 5 d 5
+5 [of] [of] [of] [6f] u [ps] e 6 [es] [us] [ep] [4g] q u q 4 q u
+h [8sf] h [osf] [8h] [osf] h [8of] d g 5 9 w p g w [of] w
+
+[6us] 0 [ts] e [us] [tf] [sfl] [adk] [4sj] 8 [ej] [qp]
+[tj] f [th] d [8of] w [uf] t [oh] [ug] [tf] d [5oa] 9 r [wo]
+[oag] r [wof] d [6us] 0 [ts] e [us] [tf] [sfl] [adk] [4sj] 8 [ej] [qp]
+[tj] [ef] [yh] d [8of] w [uf] t [oh] [ug] [tf] d [5od] 9 r [wo]
+[oag] r [wof] d 6 0 e t o ]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KaysiRB/NovaHUB/refs/heads/main/SCRIPT/HomeMadePiano.lua"))()
+Window:Notify({
+    Title = "Nova HUB",
+    Description = "Faded by Alan Walker has been finished.",
+    Lifetime = 3
+})
+        	end,
 		})
 
-local TabGroup5 = Window:TabGroup()
-local SettingsTab = TabGroup5:Tab({
+local TabGroup4 = Window:TabGroup()
+local SettingsTab = TabGroup4:Tab({
     Name = "Settings",
     Image = "rbxassetid://6034509993" -- Image can be at maximum 16 pixels wide and 16 pixels tall.
 })
@@ -246,3 +299,8 @@ Window:Notify({
 })
 MacLib:SetFolder("NovaHUB")
 MacLib:LoadAutoLoadConfig()
+
+-- Mettre à jour le vol à chaque frame
+game:GetService("RunService").RenderStepped:Connect(function()
+    FlyModule:updateFlight()  -- Met à jour la position et l'orientation du joueur en vol
+end)
