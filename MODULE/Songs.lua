39b6f51fbbb394bfa2c940794819e4519f7f8466
Nova / Kays <165211324+KaysiRB@users.noreply.github.com>
2024-12-20 00:10:30 +0100

    Create Songs.lua

---
+++ MODULE/Songs.lua
@@ -0,0 +1,30 @@
+-- Module: Songs
+local Songs = {}
+
+Songs["Faded"] = {
+    ftime = 120,
+    scr = [[
+        [6s] 0 [ts] e [us] t [of] u [4j] 8 [ej] q [tj] e [yh] u
+        [8f] w [uf] t [of] u [tf] w [5a] 9 [ra] w [ya] r [wp] 0
+        ...
+    ]]
+}
+
+Songs["Blue bird"] = {
+    ftime = 120,
+    scr = [[
+        f j k [el] t u p [wk] r y [oj] e t u p
+        s f j k [el] t u p [wz] r [yl] [oz] 0 [Wx] r u
+        ...
+    ]]
+}
+
+Songs["Rush E"] = {
+    ftime = 180,
+    scr = [[
+        u u u u u u u u u u u u u u u u u u u u u u u u u u u
+        ...
+    ]]
+}
+
+return Songs
